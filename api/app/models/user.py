ae86f512e271b6ac9786ef5969731f5310d7d015
dExNight <am.gubaidullin@innopolis.university>
2024-12-05 16:16:29 +0300

    refactor: api/

---
+++ api/app/models/user.py
@@ -0,0 +1,14 @@
+from app import db
+from datetime import datetime
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    is_active = db.Column(db.Boolean, default=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
