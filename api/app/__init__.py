a8b30e7344f5f14cc153c5e6cf792ff2edc4be8a
dExNight <am.gubaidullin@innopolis.university>
2024-12-05 18:18:12 +0300

    feat: cors

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
@@ -12,7 +13,15 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
+    
+    CORS(app, resources={
+        r"/api/*": {
+            "origins": ["http://localhost:5174"],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type", "Authorization"]
+        }
+    })
+    
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
